95dd3c8faf9262ff286a1862f4d1dcbc021e1c9b
QuanDaoTechnology <alvintrantechnology@gmail.com>
2025-11-18 01:44:10 +0700

    Update tailwind.config,js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Dòng này quan trọng
+  ],
+  theme: {
+    extend: {
+      // Thêm font Poppins bạn đã dùng ở index.html
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
