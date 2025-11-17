1befd88a07d54139bd75b93639266367e975d769
QuanDaoTechnology <alvintrantechnology@gmail.com>
2025-11-18 01:48:39 +0700

    Update app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css' // Đảm bảo bạn import file index.css
-import { BrowserRouter } from 'react-router-dom' // Import
+import { BrowserRouter } from 'react-router-dom' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* Bọc App bằng BrowserRouter */}
+
     <BrowserRouter>
       <App />
     </BrowserRouter>
