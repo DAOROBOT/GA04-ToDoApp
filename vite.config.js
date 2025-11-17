1a17489377b0868a1630bec804735197fb1f10f7
QuanDaoTechnology <alvintrantechnology@gmail.com>
2025-11-18 02:09:25 +0700

    Create docs folder in order to deploy successfully to Github Page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const REPO_NAME = 'GA04-ToDoApp'; 
+
 export default defineConfig({
   plugins: [react()],
+  
+
+  base: `/${REPO_NAME}/`, 
+  
+ 
+  build: {
+  
+    outDir: 'docs', 
+
+    emptyOutDir: true, 
+  }
 })
